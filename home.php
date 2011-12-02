0b6996dd005d45ef4f47e60872e87e4f57f9c177
aakash <aakashv2.1@gmail.com>
2011-12-01 23:51:06 -0500

    home page changed for admin

--- home.php
+++ home.php
@@ -19,12 +19,42 @@
 
 <body>
 	<br /></br >
-	<table class = "transactions" align = "center" border = "1" width = "50%">
+	<?php
+	if ('admin@mmt.com' != $_SESSION['email'])
+	{
+	?>
+		<table class = "transactions" align = "center" border = "1" width = "50%">
+	<?php
+	}
+	else
+	{
+	?>
+		<table class = "transactions" align = "center" border = "1" width = "30%">
+	<?php
+	}
+	?>
 		<tr>
-			<td>Transactions</td>
-			<td>Other</td>
+			<?php
+				if ('admin@mmt.com' != $_SESSION['email'])
+				{
+			?>
+				<td>Transactions</td>
+				<td>Other</td>
+			<?php
+				}
+			else
+				{
+			?>
+				<td>Perform database modifications</td>
+			<?php
+				}
+			?>
 		</tr>
 		<tr>
+			<?php
+				if ('admin@mmt.com' != $_SESSION['email'])
+				{
+			?>
 			<td valign = "top">
 				<a href = "addTransaction.php">Add Transaction</a> <br />
 				<a href = "viewTransactions.php">View Transactions</a> <br />
@@ -34,6 +64,19 @@
 				<a href = "modFriends.php">Modify Friends</a> <br />
 				<a href = "group.php">Modify Groups</a> <br />
 			</td>
+			<?php
+			}
+			else
+			{
+			?>
+			<td valign = "top">
+				<a href = "delUsers.php">Delete Users</a> <br />
+				<a href = "delTransactions.php">Delete Transactions</a> <br />
+				<a href = "delGroups.php">Delete Groups</a> <br />
+			</td>
+			<?php
+			}
+			?>
 		</tr>
 	</table>
 </body>
